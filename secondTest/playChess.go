e52e91ba861bea64fbd1230ddf01579cc252cf14
Zhou Junjie <zhoujunjie@Zhous-MacBook-Pro.local>
2019-04-23 20:15:58 +0800

    submit first 2 tests

--- secondTest/playChess.go
+++ secondTest/playChess.go
@@ -1,9 +1,6 @@
 package main
 
 import(
-	"fmt"
-	"math/rand"
-	"time"
 	ds "github.com/Ankr/datastructure"
 )
 
@@ -21,73 +18,22 @@ func main(){
 
 // Functions below are implemented for the 2nd test.
 func playChess(){
-	cb := ds.Chessboard{
-		{square.Empty, square.Empty, square.Empty, square.Empty, square.Empty, square.Empty, square.Empty},
-		{square.Empty, square.Empty, square.Empty, square.Empty, square.Empty, square.Empty, square.Empty},
-		{square.Empty, square.Empty, square.Empty, square.Empty, square.Empty, square.Empty, square.Empty},
-		{square.Empty, square.Empty, square.Empty, square.Empty, square.Empty, square.Empty, square.Empty},
-		{square.Empty, square.Empty, square.Empty, square.Empty, square.Empty, square.Empty, square.Empty},
-		{square.Empty, square.Empty, square.Empty, square.Empty, square.Empty, square.Empty, square.Empty},
-   }
-   hasempty := true
-   for hasempty {
+   cb := &ds.Chessboard{}
+   for cb.HasEmpty() {
 		cb = player1Term(cb)
-		if(hasEmpty(cb)){
+		if(cb.HasEmpty()){
 			cb = player2Term(cb)
 		}
-		hasempty = hasEmpty(cb)
    }
-   printChessboard(cb)
+   cb.PrintChessboard()
 }
 
-func player1Term(cb ds.Chessboard) ds.Chessboard {
-	refereshedChessboard := play(cb, square.Black)
-	return refereshedChessboard
-}
-
-func player2Term(cb ds.Chessboard) ds.Chessboard {
-	refereshedChessboard := play(cb, square.White)
-	return refereshedChessboard
-}
-
-func play(cb ds.Chessboard, piece int) ds.Chessboard{
-	// randomly choose one column
-	rand.Seed(time.Now().UnixNano())
-	index := rand.Intn(7)
-	// form bottom to top
-	for i := 5; i >= 0; i-- {
-		if ( cb[i][index] == 0 ){
-			cb[i][index] = piece
-			return cb
-		}
-	}
+func player1Term(cb *ds.Chessboard) *ds.Chessboard {
+	cb.Play(square.Black)
 	return cb
 }
 
-func hasEmpty(cb ds.Chessboard) bool{
-	// from top to bottom
-	for _, rowContent := range cb {
-		for _, component := range rowContent {
-			if ( component == 0 ){
-				return true
-			}
-		}
-	}
-	return false
-}
-
-func printChessboard(cb ds.Chessboard){
-	for _, rowcontent := range cb	{
-		for _, rowcomponent  := range rowcontent {
-			switch rowcomponent{
-			case 0:
-				fmt.Printf("%v\t", ".")	
-			case 1:
-				fmt.Printf("%v\t", "x")
-			case 2:
-				fmt.Printf("%v\t", "o")
-			}
-		}
-		fmt.Println()
-	}
+func player2Term(cb *ds.Chessboard) *ds.Chessboard {
+	cb.Play(square.White)
+	return cb
 }
